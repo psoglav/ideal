f5e850eb1917030ece12c57f40e35c10b54968b1
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-03-13 02:06:53 +0600

    dealer sketch

---
+++ src/components/connection/Dealers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dealers-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dealers'
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"><Dealer status="staring" /></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Dealer from '@/components/connection/Dealer'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Dealer },
 }
 </script>
