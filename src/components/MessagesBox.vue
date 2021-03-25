a12d032bffe3f3af3dda6375cc833a8c72809fa4
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-03-25 20:07:25 +0600

    f-end: moved messages to store

--- src/components/MessagesBox.vue
+++ src/components/MessagesBox.vue
@@ -1,6 +1,6 @@
 p<template>
   <div class="container">
-    <div class="container_messages">
+    <div ref="messagesContainer" class="container_messages">
       <div
         v-for="(stack, i) in stacks"
         :key="i"
@@ -37,98 +37,6 @@ export default {
   data() {
     return {
       stacks: [],
-      messages: [
-        {
-          content: {
-            text:
-              'Query parameters can also be provided, either with the query option or directly in the url (example: http://localhost/users?token=abc).',
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 1,
-          at: +new Date() + 63510 * 3,
-        },
-        {
-          content: {
-            text:
-              'Creates a new Manager for the given URL, and attempts to reuse an existing Manager for subsequent calls, unless the multiplex option is passed with false. Passing this option is the equivalent of passing "force new connection": true or forceNew: true.',
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 8932489,
-          at: +new Date() + 61394 * 7,
-        },
-        {
-          content: {
-            text:
-              'A new Socket instance is returned for the namespace specified by the pathname in the URL, defaulting to /. For example, if the url is http://localhost/users, a transport connection will be established to http://localhost and a Socket.IO connection will be established to /users.',
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 8932489,
-          at: +new Date() + 61396 * 7,
-        },
-        {
-          content: {
-            text:
-              "Hay friends I was trying to make this code. Actually in this code there is no error according to console but I am facing a problem I am not able to control perspective origin and perspective with javascript. If anyone knows why it's not working then I will be very thankful to him/her. I had done many of successful code with same pattern as in this code they all are working but I failed to found error in this code. Before giving answer its humble request that first change do the experiment for yourself by editing code and then give me answer.\n\nNote : I dont want to use css because it is like a controller and if I will use css for this it will not be like a controller anymore.",
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 8932489,
-          at: +new Date() + 67400 * 8,
-        },
-        {
-          content: {
-            text:
-              'Offload aged data from your Atlas cluster to a lower-cost, queryable storage tier. Create an Online Archive to start saving on storage costs.',
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 1,
-          at: +new Date() + 67400 * 8,
-        },
-        {
-          content: {
-            text:
-              'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            attachments: [
-              {
-                type: 'image',
-                url:
-                  'https://sun9-61.userapi.com/impg/AAUWN4BdMpMFYZ2P4IcPAGmKHD85z2PoPdwBWw/uk5z5GJFvQs.jpg?size=563x751&quality=96&sign=eb6853c14b4ee76fc5517e2de382513c&type=album',
-              },
-            ],
-          },
-          user: 1,
-          at: +new Date() + 67400 * 8,
-        },
-      ],
     }
   },
   watch: {
@@ -155,6 +63,11 @@ export default {
             this.stacks[l - 1].push(next)
           }
         }
+
+        // const el = this.$refs.messagesContainer
+        // setTimeout(() => {
+        // el.scrollTop = 2000
+        // }, 100)
       },
     },
   },
@@ -164,6 +77,9 @@ export default {
     },
     user() {
       return this.$store.getters.user
+    },
+    messages() {
+      return this.$store.getters.messages
     },
   },
   methods: {
@@ -321,12 +237,14 @@ $msg-hover-time: 0.4s;
 
       &_time {
         left: 0;
-        bottom: -0.4rem;
+        bottom: -0.2rem;
         position: absolute;
         text-align: left;
+        text-transform: uppercase;
         font-size: 0.8rem;
         opacity: 0;
         width: 100%;
+        font-family: 'Open Sans', Montserrat, sans-serif;
         transition: opacity 0.1s;
         pointer-events: none;
 
@@ -334,7 +252,7 @@ $msg-hover-time: 0.4s;
         .collapsed {
           display: block;
           position: absolute;
-          transition: opacity 0.2s;
+          transition: opacity 0.4s;
           top: 0;
         }
         .expanded {
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import participants from './modules/participants'
+import messagesBox from './modules/messagesBox'
 
 Vue.use(Vuex)
 
@@ -13,6 +14,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    participants
+    participants,
+    messagesBox
   }
 })
